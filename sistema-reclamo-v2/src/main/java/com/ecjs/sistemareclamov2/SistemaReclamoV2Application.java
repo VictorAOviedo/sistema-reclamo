a4d4a88700d22acfbe10ce80346e6e3ed4424425
Victor Oviedo <victor.oviedo.a87@gmail.com>
2023-08-24 15:27:00 -0300

    spring initializr

---
+++ sistema-reclamo-v2/src/main/java/com/ecjs/sistemareclamov2/SistemaReclamoV2Application.java
@@ -0,0 +1,13 @@
+package com.ecjs.sistemareclamov2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaReclamoV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaReclamoV2Application.class, args);
+	}
+
+}
---
+++ sistema-reclamo-v2/src/test/java/com/ecjs/sistemareclamov2/SistemaReclamoV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecjs.sistemareclamov2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaReclamoV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
